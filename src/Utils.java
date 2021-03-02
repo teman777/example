ab5554ce6894828cd2d2aee2bbed1b772808905f
Artem Voronov <artemkavoronov@gmail.com>
2021-03-02 21:28:23 +0400

    Сделал создание объекта копии - выглядит криво, но в дебаггере зато красиво :)

--- src/Utils.java
+++ src/Utils.java
@@ -1,21 +1,41 @@
+import com.sun.jdi.InterfaceType;
+
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
 
     public static Object deepCopy(Object obj){
-        Class a = null;
+        Class<?> clazz = obj.getClass();
+        List<String> l = new ArrayList<>();
+        Object res = null;
         try {
-            a = Class.forName(obj.getClass().getName());
-        } catch (ClassNotFoundException e) {
+            res = clazz.getDeclaredConstructor(String.class, int.class, List.class).newInstance("1", 2, l);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
-        Object obj2 = null;
+
+        Field[] fields = clazz.getDeclaredFields();
+        for(Field field: fields){
+
+        }
 
 
 
 
-        return obj2;
+
+
+
+        return obj;
 
     }
 
